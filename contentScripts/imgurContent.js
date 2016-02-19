1e07c61d68f4f769c5fe3dd7cbf0fcfa4f536492
Johnathan Sult <johnathansult@gmail.com>
2016-02-19 11:50:07 -0800

    Added a comment to remind myself of how to improve onCommentsLoaded() behavior.

--- contentScripts/imgurContent.js
+++ contentScripts/imgurContent.js
@@ -35,8 +35,10 @@ var mutationObserver = new MutationObserver( function(mutations) {
 				}, 10);
 				//The following node classNames all change once per new post: humanMsg, point-info left bold, views-info left
 			}
-			else if((mut.addedNodes[j].className.indexOf("comment") > -1 || mut.addedNodes[j].className.indexOf("children") > -1) && mut.addedNodes[j].className != "favorite-comment")
+            else if((mut.addedNodes[j].className.indexOf("comment") > -1 || mut.addedNodes[j].className.indexOf("children") > -1) && mut.addedNodes[j].className != "favorite-comment") {
+                // TODO: Convert this from a blanket onCommentsLoaded() call to executing the contents of that call on the nodeList at mut.addedNodes[j].
 				onCommentsLoaded();
+            }
 		}
 	}
 } );
